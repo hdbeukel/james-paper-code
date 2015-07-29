1ca57d801444e1fa3a23b255d5684b0e7ff74370
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2015-07-29 09:50:36 +0200

    try with max temp 1.0

--- james-tsp/src/main/java/org/jamesframework/tsp/Solver.java
+++ james-tsp/src/main/java/org/jamesframework/tsp/Solver.java
@@ -36,69 +36,69 @@ public class Solver {
         String filePath = args[2];
         run(filePath, runs, timeLimit);
     }
-    
+
     private static void run(String filePath, int runs, int timeLimit){
-        
+
         try {
-            
+
             // read input
             TSPFileReader reader = new TSPFileReader();
             TSPData data = reader.read(filePath);
-            
+
             // create objective
             TSPObjective obj = new TSPObjective();
-            
+
             // create problem
             Problem<TSPSolution> problem = new GenericProblem<>(data, obj, TSP.RANDOM_SOLUTION_GENERATOR);
-            
+
             // create stop criterion
             StopCriterion sc = new MaxRuntime(timeLimit, TimeUnit.SECONDS);
-            
+
             // write header
             System.out.println("repeat, rd, pt");
-            
+
             // repeatedly apply random descent and parallel tempering
             for(int repeat = 0; repeat < runs; repeat++){
-                
+
                 // create random descent
                 Search<TSPSolution> rd = API.getAPI().getRandomDescent(problem);
                 rd.addStopCriterion(sc);
-                
+
                 // apply random descent
                 rd.start();
-                
+
                 // store best value
                 double rdBestValue = rd.getBestSolutionEvaluation().getValue();
-                
+
                 // dispose random descent
                 rd.dispose();
 
                 // create parallel tempering
                 double minTemp = 1e-8;
-                double maxTemp = 0.6;
+                double maxTemp = 1.0;
                 int numReplicas = 10;
                 Search<TSPSolution> pt = API.getAPI().getParallelTempering(problem, minTemp, maxTemp, numReplicas);
                 pt.addStopCriterion(sc);
-                
+
                 // apply parallel tempering
                 pt.start();
-                
+
                 // store best value
                 double ptBestValue = pt.getBestSolutionEvaluation().getValue();
-                
+
                 // dispose parallel tempering
                 pt.dispose();
-                
+
                 // write repeat number and best values
                 System.out.format(Locale.US, "%d, %d, %d\n", repeat, Math.round(rdBestValue), Math.round(ptBestValue));
-                
+
             }
-            
+
         } catch (FileNotFoundException ex) {
             System.err.println("Cannot read file: " + filePath);
             System.exit(2);
         }
-        
+
     }
-    
+
 }
