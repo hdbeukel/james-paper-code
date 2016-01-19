a45670ad4517d68163777eec3139da5f29e8bd8b
Herman De Beukelaer <herman.debeukelaer@ugent.be>
2016-01-19 19:02:58 +0100

    base EvA2 implementation of core selection problem

---
+++ eva2/src/main/java/org/jamesframework/eva2/CoreSelectionProblem.java
@@ -0,0 +1,89 @@
+
+package org.jamesframework.eva2;
+
+import eva2.optimization.individuals.AbstractEAIndividual;
+import eva2.optimization.individuals.InterfaceDataTypeBinary;
+import eva2.optimization.population.Population;
+import eva2.problems.AbstractProblemBinary;
+import java.util.BitSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.jamesframework.core.util.SetUtilities;
+
+public class CoreSelectionProblem extends AbstractProblemBinary {
+
+    // random generator
+    private static final Random RNG = new Random();
+    
+    // size of the dataset (number of individuals)
+    private final int n;
+    // size of the core collection
+    private final int coreSize;
+    // set of indices assigned to items in bitset
+    private final Set<Integer> indices;
+    
+    // distance matrix
+    private final double[][] dist;
+    
+    public CoreSelectionProblem(int n, int coreSize, double[][] dist) {
+        this.n = n;
+        this.coreSize = coreSize;
+        this.dist = dist;
+        indices = IntStream.range(0, n).boxed().collect(Collectors.toSet());
+    }
+    
+    @Override
+    public double[] evaluate(BitSet t) {
+
+        int s = t.cardinality();
+        int numDist = s * (s - 1) / 2;
+        double sumDist = 0.0;
+        int[] sel = t.stream().toArray();
+        for (int i = 0; i < s; i++) {
+            for (int j = i + 1; j < s; j++) {
+                sumDist += dist[sel[i]][sel[j]];
+            }
+        }
+        
+        // maximize average distance
+        return new double[] {-sumDist/numDist};
+        
+    }
+
+    @Override
+    public int getProblemDimension() {
+        return n;
+    }
+
+    @Override
+    public Object clone() {
+        CoreSelectionProblem copy = new CoreSelectionProblem(n, coreSize, dist);
+        copy.cloneObjects(this);
+        return copy;
+    }
+
+    @Override
+    public void initializePopulation(Population population) {
+        
+        super.initializePopulation(population);
+        
+        // modify solutions to match desired core size
+        for(AbstractEAIndividual ind : population){
+            InterfaceDataTypeBinary indBinary = (InterfaceDataTypeBinary) ind;
+            BitSet b = indBinary.getBinaryData();
+            // generate random subset of indices to set
+            Set<Integer> subset = SetUtilities.getRandomSubset(indices, coreSize, RNG);
+            // modify bits in bitset
+            b.clear();
+            subset.forEach(i -> b.set(i));
+            // set modified genotype
+            indBinary.setBinaryGenotype(b);
+        }
+        
+    }
+    
+    
+    
+}
---
+++ eva2/src/main/java/org/jamesframework/eva2/SubsetSwapMutation.java
@@ -0,0 +1,61 @@
+
+package org.jamesframework.eva2;
+
+import eva2.optimization.individuals.AbstractEAIndividual;
+import eva2.optimization.individuals.InterfaceDataTypeBinary;
+import eva2.optimization.operator.mutation.InterfaceMutation;
+import eva2.optimization.population.Population;
+import eva2.problems.InterfaceOptimizationProblem;
+import java.util.BitSet;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SubsetSwapMutation implements InterfaceMutation {
+
+    private static final Random RG = new Random();
+    private final int n;
+
+    public SubsetSwapMutation(int n) {
+        this.n = n;
+    }
+    
+    @Override
+    public void initialize(AbstractEAIndividual individual, InterfaceOptimizationProblem opt) {}
+
+    @Override
+    public void mutate(AbstractEAIndividual individual) {
+    
+        InterfaceDataTypeBinary bInd = (InterfaceDataTypeBinary) individual;
+        BitSet b = bInd.getBinaryData();
+        
+        // swap one selected and unselected item
+        List<Integer> sel = b.stream().boxed().collect(Collectors.toList());
+        List<Integer> unsel = IntStream.range(0, n).boxed()
+                                                          .filter(i -> !sel.contains(i))
+                                                          .collect(Collectors.toList());
+        int add = unsel.get(RG.nextInt(unsel.size()));
+        int del = sel.get(RG.nextInt(sel.size()));
+        
+        b.flip(add);
+        b.flip(del);
+        
+        bInd.setBinaryGenotype(b);
+        
+    }
+
+    @Override
+    public void crossoverOnStrategyParameters(AbstractEAIndividual indy1, Population partners) {}
+
+    @Override
+    public String getStringRepresentation() {
+        return "Subset swap mutation";
+    }
+    
+    @Override
+    public Object clone(){
+        return new SubsetSwapMutation(n);
+    }
+
+}
